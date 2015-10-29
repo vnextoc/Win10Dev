2f3f3520f6c6b6bc50f315110731184d7e641e51
Jeff Hart <vnextoc949@gmail.com>
2015-10-28 20:38:50 -0700

    Added properties to list view. Start on UI.

---
+++ MobbedUpApp/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobbedUpApp {
+    public static class Utilities {
+        public static DateTime JavascriptDate(this double milliseconds) {
+            return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                .AddMilliseconds(milliseconds);
+        }
+    }
+}
--- MobbedUpApp/ViewModel/MainPageViewModel.cs
+++ MobbedUpApp/ViewModel/MainPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 
 namespace MobbedUpApp.ViewModel 
 {
@@ -10,7 +11,10 @@ namespace MobbedUpApp.ViewModel
             var data = new DAL.SampleRepository();
             var results = data.GetResults;
             foreach( var item in results ) {
-                Items.Add(new MeetupViewModel { Title = item.name});
+                Items.Add(new MeetupViewModel { Title = item.name,
+                    Address = $"{item.venue?.address_1}\r\n{item.venue?.city}, {item.venue?.state}",
+                    StartTime = Convert.ToDouble(item.time).JavascriptDate().ToLocalTime()
+                });
             }
 
         }
@@ -22,6 +26,7 @@ namespace MobbedUpApp.ViewModel
     public class MeetupViewModel 
     {
         public string Title { get; set; }
-
+        public string Address { get; set; }
+        public DateTime StartTime { get; set; }
     }
 }
